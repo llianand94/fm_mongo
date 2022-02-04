2a48bf0e1c305a31af77e49fc0d824c58a09cdd9
Andrey Shapovalov <llianandr94@gmai.com>
2022-02-04 11:35:56 +0200

    Add handlers POST and GET requests

--- server.js
+++ server.js
@@ -1,12 +1,51 @@
 const http = require('http');
 const express = require('express');
 const yup = require('yup');
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+mongoose
+.connect('mongodb://localhost:27017/fm_mongoose')
+.catch(error => console.log(error));
+
+const tastSchema = new Schema({
+  title: String,
+  date: { type: Date, default: Date.now },
+  isDone: Boolean,
+  author: {
+    name:String,
+    age: Number
+  }
+});
+
+
+
+const Task = mongoose.model('Task', tastSchema);
+
+const getMethod = async (req,res,next)=>{
+  try{
+    const tasks = await Task.find();
+    res.status(200).send(tasks);
+  }catch(error){
+    next(error);
+  }
+};
+const postMethod = async (req,res,next)=>{
+  try{
+    const {body} = req; 
+    const newTask = await Task.create(body);
+    res.status(201).send(newTask);
+  }catch(error){
+    next(error);
+  }
+}
 
 const app = express();
 app.use(express.json());
+app.post('/', postMethod);
+app.get('/', getMethod);
+
 
 const server = http.createServer(app);
-
 const PORT = process.env.PORT || 3000;
-
 server.listen(PORT,()=>{console.log("Server started at port " + PORT)});
