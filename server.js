9b64c97b7ef4ba3bf0b7534b78d3b1394282974f
Andrey Shapovalov <llianandr94@gmai.com>
2022-02-10 09:36:01 +0200

    Add prettier-standard JS

--- server.js
+++ server.js
@@ -5,147 +5,160 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const host = 'fm_mongo';
 
-
-mongoose
-.connect(`mongodb://${host}:27017/fm_mongoose`)
-.catch(error => {
+mongoose.connect(`mongodb://${host}:27017/fm_mongoose`).catch(error => {
   console.log(error);
-  process.exit(1)
+  process.exit(1);
 });
 
-const emailSchema = yup.string().email().required();
+const emailSchema = yup
+  .string()
+  .email()
+  .required();
 
-const tastSchema = new Schema({
-  title: {
-    type: String, 
-    require: [true, "Task have to be"],
-    validate:{
-      validator: (v)=>/[A-Z][a-z\s]{4,200}/.test(v),
-      message: '{VALUE} must be letter'
-    }
-  },
-  date: { type: Date, default: Date.now },
-  isDone: { type: Boolean, default: false },
-  author: {
-    name:{
-      type: String, 
-      require: true
+const tastSchema = new Schema(
+  {
+    title: {
+      type: String,
+      require: [true, 'Task have to be'],
+      validate: {
+        validator: v => /[A-Z][a-z\s]{4,200}/.test(v),
+        message: '{VALUE} must be letter',
+      },
     },
-    email:{
-      type:String,
-      required:true,
-      validate:{
-        validator:(v)=>emailSchema.isValid(v)
-      } 
+    date: { type: Date, default: Date.now },
+    isDone: { type: Boolean, default: false },
+    author: {
+      name: {
+        type: String,
+        require: true,
+      },
+      email: {
+        type: String,
+        required: true,
+        validate: {
+          validator: v => emailSchema.isValid(v),
+        },
+      },
+      age: {
+        type: Number,
+        validate: {
+          validator: v => v > 0,
+        },
+      },
     },
-    age: {
-      type: Number,      
-      validate:{
-        validator: (v)=>v>0
-      } 
-    }
-  }
-},{
-  versionKey: false,
-  timestamps: true
-});
-
-const commentsSchema = new Schema({
-  title:{
-    type: String,
-    required:true
   },
-  taskLink: {
-    type: Schema.Types.ObjectId, ref: 'Task',
-    required:true
+  {
+    versionKey: false,
+    timestamps: true,
   }
-},
-{
-  versionKey: false,
-  timestamps: true
-})
+);
 
-
-
+const commentsSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    taskLink: {
+      type: Schema.Types.ObjectId,
+      ref: 'Task',
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 
 const Task = mongoose.model('Task', tastSchema);
 const Comment = mongoose.model('Comment', commentsSchema);
 
-
-const getMethod = async (req,res,next)=>{
-  try{
+const getMethod = async (req, res, next) => {
+  try {
     const tasks = await Task.find();
     res.status(200).send(tasks);
-  }catch(error){
+  } catch (error) {
     next(error);
   }
 };
-const postMethod = async (req,res,next)=>{
-  try{
-    const {body} = req; 
+const postMethod = async (req, res, next) => {
+  try {
+    const { body } = req;
     const newTask = await Task.create(body);
     res.status(201).send(newTask);
-  }catch(error){
+  } catch (error) {
     next(error);
   }
-}
-const updateMehtod = async (req,res,next)=>{
-  try{
-    const {body, params:{taskId}}= req;
-    const updatedTask = await Task.findOneAndUpdate({_id:taskId}, body,{returnDocument:'after'});
+};
+const updateMehtod = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { taskId },
+    } = req;
+    const updatedTask = await Task.findOneAndUpdate({ _id: taskId }, body, {
+      returnDocument: 'after',
+    });
     res.status(200).send(updatedTask);
-  }catch(error){
+  } catch (error) {
     next(error);
   }
-}
-const deleteTask = async (req,res,next)=>{
-  try{
-    const {params:{taskId}}= req;
+};
+const deleteTask = async (req, res, next) => {
+  try {
+    const {
+      params: { taskId },
+    } = req;
     const deleteTask = await Task.findByIdAndRemove(taskId);
-    if(deleteTask){
-      res.status(200).send(deleteTask)
+    if (deleteTask) {
+      res.status(200).send(deleteTask);
     }
     res.status(404).send();
-  }catch(error){
+  } catch (error) {
     next(error);
   }
-}
+};
 
-const postComment = async (req,res,next)=>{
-  try{
-    const {body, params:{taskId}}= req;
-    const newComment = await Comment.create({...body, taskLink:taskId})
+const postComment = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { taskId },
+    } = req;
+    const newComment = await Comment.create({ ...body, taskLink: taskId });
     res.status(200).send(newComment);
-  }catch(error){
+  } catch (error) {
     next(error);
   }
-}
-const getAllComments = async (req, res, next)=>{
-  try{
+};
+const getAllComments = async (req, res, next) => {
+  try {
     Comment.find()
-    .populate('taskLink')
-    .exec((error,comments)=>{
-      if(error){
-        throw error
-      }
-      res.send(comments);
-    })    
-  }catch(error){
+      .populate('taskLink')
+      .exec((error, comments) => {
+        if (error) {
+          throw error;
+        }
+        res.send(comments);
+      });
+  } catch (error) {
     next(error);
   }
-}
+};
 
 const app = express();
 app.use(express.json());
 app.post('/', postMethod);
 app.get('/', getMethod);
-app.patch('/:taskId', updateMehtod)
+app.patch('/:taskId', updateMehtod);
 app.delete('/:taskId', deleteTask);
 
-app.post('/:taskId/comments/',postComment);
+app.post('/:taskId/comments/', postComment);
 app.get('/comments/', getAllComments);
-
 
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8080;
-server.listen(PORT,()=>{console.log("Server started at port " + PORT)});
+server.listen(PORT, () => {
+  console.log('Server started at port ' + PORT);
+});
